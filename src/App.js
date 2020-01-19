8433e89f96292d775ca44e2699523b5d4d0d1361
Silvio Quenta <silvioqc12@gmail.com>
2020-01-19 00:02:55 -0500

    Clase 2 Aprendiendo JSX con React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss"
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+<>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" src="https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500.jpg"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Título del banner</p>
+          <p> Subtítulo del banner</p>
+          <a href="https://ed.team" className="button">Botón del banner</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+  <div className="ed-grid m-grid-3">
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+  </div>
+</>
+)
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Reglas
+//1: Toda etiqueta debe cerrarse
+//2: Los componentes deben devolver UN solo elemento padre
+//3: Apoyarse de los Fragment cuando  necesito devolver 2 elementos
+//4: Frangment => <> hijos </>
+//5: img siempre se cierra
+//6: class => className
+//7: for => htmlFor
+
