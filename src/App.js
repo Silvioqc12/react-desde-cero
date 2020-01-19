9ce63eb7c6c789349bd6aca90252ba06f5e89be3
Silvio Quenta <silvioqc12@gmail.com>
2020-01-19 15:57:05 -0500

    terminado la segunda parte de curso react.js con EDteam

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React from 'react';
 import "./styles/styles.scss"
 import Curso from './Curso'
 
+const Cursos = [
+  {
+    "titulo": "React desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    "price": "30",
+    "profesor": "Beto Quiroga"
+  }, {
+    "titulo": "Drupal desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/drupal-theming-poster_0.png?itok=9bzhElpl",
+    "price": "60",
+    "profesor": "Beto Quiroga"
+  }, {
+    "titulo": "Go desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/go_0.jpg?itok=k2amLhrN",
+    "price": "50",
+    "profesor": "Alexis Losada"
+  }, {
+    "titulo": "HTML desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/pwa-poster_1.png?itok=CmNPn3bq",
+    "price": "10",
+    "profesor": " Alvaro Felipe"
+  } 
+  , {
+    "titulo": "Fundamento de redes",
+    "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-03/Redes%20Fundamentos.png",
+    "price": "40",
+    "profesor": "Alexis Losada"
+  } 
+  , {
+    "titulo": "Android Base de datos",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/imagenes-cdn-edteam/2019-01/Android%20Bases%20de%20datos.png?itok=3pW_l_7C",
+    "price": "50",
+    "profesor": "Alvaro Felipe"
+  } 
+]
+
 const App = () => (
 <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -9,24 +45,18 @@ const App = () => (
       <div className="lg-cols-4 lg-x-2">
         <img className="main-banner__img" src="https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500.jpg"/>
         <div className="main-banner__data s-center">
-          <p className="t2 s-mb-0">Título del banner</p>
-          <p> Subtítulo del banner</p>
-          <a href="https://ed.team" className="button">Botón del banner</a>
+          <p className="t2 s-mb-0">Curso de EDteam</p>
+          <p> Tu futuro te esta esperando</p>
+          <a href="https://ed.team" className="button">Suscribite</a>
         </div>
       </div>
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
+    {
+      Cursos.map( c => <Curso title={c.titulo} image={c.image} price={c.price} profesor={c.profesor} /> )
+    }
+    
   </div>
 </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Curso = () => (
+const Curso = ({title, image, price, profesor}) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://res.cloudinary.com/edteam/image/upload/w_400/v1573259688/courses/bi-poster.png" alt=""/>
+            <img src={image} alt={title}/>
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="t5 s-mb-2 s-center">
-                Programación orientada a objetos con Go
-            </h3>
-            <div className="s-mb-2 s-main-center">
-                <div className="card__teacher s-cross-center">
-                    <div className="card__avatar s-mr-1">
-                        <div className="circle img-container">
-                            <img src="https://drupal.ed.team/sites/default/files/styles/thumbnail/public/imagenes-cdn-edteam/2019-11/haz.jpg" alt=""/>
-                        </div>
-                    </div>
-                    <span className="small">Alexys Lozada</span>
-                </div>
+            <h3 className="center">{title}</h3>
+            <div className="s-main-center">
+                {profesor}
             </div>
             <div className="s-main-center">
-                <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+                <a className="button--ghost-alert button--tiny" href="#">{`$ ${price} USD`}</a>
             </div>
         </div>
     </article>
 )
+
+Curso.prototype = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.number,
+    profesor: PropTypes.string
+}
+Curso.defaultProps = {
+    title: "no se encontró titulo",
+    image: "https://st2.depositphotos.com/3139517/9378/i/950/depositphotos_93782974-stock-photo-hong-kong-city-and-buildings.jpg",
+    price: "--",
+    profesor: ""
+}
+
 
 export default Curso;
