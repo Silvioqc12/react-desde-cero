5b50d4024569f815fa0809b7a1350e961356ac2f
Silvio Quenta <silvioqc12@gmail.com>
2020-01-20 14:21:31 -0500

    Teminando la tercera parte del curso de react.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import "./styles/styles.scss"
-import Curso from './Curso'
-
+import Curso from "./Curso"
+import Banner from "./Banner"
+import Formulario from "./Formulario"
 
 
 const App = () => (
 <>
-  <div className="main-banner img-container l-section" id="main-banner">
-    <div className="ed-grid lg-grid-6">
-      <div className="lg-cols-4 lg-x-2">
-        <img className="main-banner__img" src="https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500.jpg"/>
-        <div className="main-banner__data s-center">
-          <p className="t2 s-mb-0">Curso de EDteam</p>
-          <p> Tu futuro te esta esperando</p>
-          <a href="https://ed.team" className="button">Suscribite</a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Banner />
+  <Formulario name="EDteam" />
 </>
 )
 
